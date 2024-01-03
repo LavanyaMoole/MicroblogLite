f8b51b18e759b1fd42a9dff64cce649a9c3f1ca6
AJCamposss <adrian.campos869@gmail.com>
2024-01-03 09:45:54 -0700

    made a little change to registration html and working on functionality

---
+++ registration/registration.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const regestration = document.getElementById("registration");
+
+});
+
+function registration(event) {
+    event.preventDefault();
+    const form = {
+        username
+    }
+}
+
+
