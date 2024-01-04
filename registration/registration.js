28319009b4dca7e5887bd31b595362d511e17ab6
AJCamposss <adrian.campos869@gmail.com>
2024-01-03 21:05:13 -0700

    working on JS

--- registration/registration.js
+++ registration/registration.js
@@ -1,14 +1,46 @@
-
-document.addEventListener("DOMContentLoaded", function() {
-    const regestration = document.getElementById("registration");
-
+document.addEventListener("DOMContentLoaded", () => {
+    const registration = document.getElementById("registration");
+    registration.addEventListener("submit",register);
 });
 
-function registration(event) {
+function register(event) {
     event.preventDefault();
-    const form = {
-        username
+
+    const username = document.getElementById("userNameInput").value;
+    const fullName = document.getElementById("fullNameInput").value;
+    const password = document.getElementById("passwordInput").value;
+
+    const userData = {
+        username: username,
+        fullName: fullName,
+        password: password
     }
-}
+   
+};
+
+fetch("http://localhost:5000/api/users", {
+    method: "POST",
+    headers: {
+        "content-Type" : "aplication/json"
+    },
+    body: JSON.stringify(userData),
+
+ 
+})
+
+.then(response => response.json())
+.then(data => {
+    window.location.href = "index.html";
+})
+
+.catch(error => {
+    console.error("Error:", error);
+})
+
+
+
+
+ 
+
 
 
