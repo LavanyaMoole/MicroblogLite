6f11a3afa311dd4c890cdc23e9d4e05cf21d8129
AJCamposss <adrian.campos869@gmail.com>
2024-01-04 09:41:39 -0700

    Still working on it

--- registration/registration.js
+++ registration/registration.js
@@ -1,40 +1,48 @@
 let userService;
 let registrationForm;
+let usernameInput;
+let fullNameInput;
+let passwordInput;
+
 
 document.addEventListener("DOMContentLoaded", () => {
-   registrationForm = document.getElementById("registration");
-    registrationForm.addEventListener("submit",registration);
+    registrationForm = document.getElementById("registration");
+    registrationForm.addEventListener("submit", registration);
+    usernameInput = document.getElementById("userNameInput");
+    fullNameInput = document.getElementById("fullNameInput");
+    passwordInput = document.getElementById("passwordInput");
+
     userService = new UserService();
 });
 
 function registration(event) {
     event.preventDefault();
 
-    
+
     const userData = {
-        username: registrationForm.usernameInput,
-        fullName: registrationForm.fullnameInput,
-        password: registrationForm.passwordInput
+        username: usernameInput.value,
+        fullName: fullNameInput.value,
+        password: passwordInput.value
     }
 
- 
+    console.log(userData)
 
 
-userService.createUser(userData)
-.then(data => {
-    window.location.href = "index.html";
-})
+        userService.createUser(userData)
+        .then(data => {
+         window.location.href = "/index.html";
+        })
 
-.catch(error => {
-    console.error("Error:", error);
-})
+       .catch(error => {
+           console.error("Error:", error);
+       })
 
 }
 
 
 
 
- 
+
 
 
 
