a274b1d5129f56e5e57b267de182de89a5c60e20
DeAsianDragon <joshlin.ling@yahoo.com>
2024-01-04 14:11:12 -0700

    land1

--- js/landing.js
+++ js/landing.js
@@ -2,10 +2,6 @@
   const doc = document
   const rootEl = doc.documentElement
   const body = doc.body
-  const sr = window.sr = ScrollReveal()
-
-  rootEl.classList.remove('no-js')
-  rootEl.classList.add('js')
 
   window.addEventListener('load', function () {
     body.classList.add('is-loaded')
